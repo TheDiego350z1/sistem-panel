7f1969b7494d200074d23c33f90104825ea1a150
Diego Antonio García <diego350z1@gmail.com>
2025-06-03 13:11:38 -0600

    fix session cookie

--- app/sessions.server.ts
+++ app/sessions.server.ts
@@ -21,8 +21,8 @@ const { getSession, commitSession, destroySession } =
       name: "__session",
 
       // all of these are optional
-      // domain: "sistem-panel.vercel.app", // defaults to the current domain
-      domain: "localhost", // defaults to the current domain
+      domain: "sistem-panel.vercel.app", // defaults to the current domain
+      // domain: "localhost", // defaults to the current domain
       // Expires can also be set (although maxAge overrides it when used in combination).
       // Note that this method is NOT recommended as `new Date` creates only one date on each server deployment, not a dynamic date in the future!
       //
